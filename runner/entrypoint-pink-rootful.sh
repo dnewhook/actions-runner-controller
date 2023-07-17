573802240bc606b7f77cb29e71998b269101bd4e
david.newhook <31928700+dnewhook@users.noreply.github.com>
2023-07-17 13:30:21 +0200

    Cleanup for PR

--- runner/entrypoint-pink-rootful.sh
+++ runner/entrypoint-pink-rootful.sh
@@ -17,7 +17,7 @@ touch /home/runner/.local/share/containers/storage/overlay-layers/layers.lock
 log.debug 'Starting Podman daemon'
 sudo podman system service --time=0 unix:///run/podman/podman.sock &
 
-log.debug 'Waiting for processes to be running...'
+log.debug 'Waiting for podman socket to be available...'
 socketspec=/run/podman/podman.sock
 
 if ! wait_for_socket "$socketspec"; then
